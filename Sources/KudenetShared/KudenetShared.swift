1e62d3e093f02ca830009f5f380a218203b44179
KudeKube <Kuutti.taavitsainen@icloud.com>
2025-05-08 21:32:07 +0300

    Update KudenetShared.swift

--- Sources/KudenetShared/KudenetShared.swift
+++ Sources/KudenetShared/KudenetShared.swift
@@ -1,2 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct TestPost {
+    var text: String
+    var owner: String
+    
+    public init() {
+        text = "empty"
+        owner = "none"
+    }
+}
